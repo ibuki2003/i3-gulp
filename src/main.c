1c4d700b1df095af630ca3da2182fab103d1f146
ibuki2003 <main@fuwa.dev>
2022-11-06 20:02:13 +0900

    remove -s support

--- src/main.c
+++ src/main.c
@@ -8,12 +8,6 @@
 #include "i3ipc.h"
 
 int main(int argc, char **argv) {
-  bool single = false;
-  if (argv[1] && strcmp(argv[1], "-s") == 0) {
-    single = true;
-    argv++;
-    --argc;
-  }
   if (argc < 2) {
     exit(0);
   }
@@ -32,15 +26,8 @@ int main(int argc, char **argv) {
 
   int win_id = 0;
 
-  int ret = 0;
   while (true) {
     bool closed = false;
-
-    int status;
-    if (waitpid(-1, &status, WNOHANG) && WIFEXITED(status)) {
-      ret = WEXITSTATUS(status);
-      break;
-    }
 
     I3ipc_event* ev_any = i3ipc_event_next(10);
     if (ev_any == NULL) continue;
@@ -66,15 +53,13 @@ int main(int argc, char **argv) {
       }
     }
     free(ev_any);
-    if (single && closed) break;
+    if (closed) break;
   }
   XCloseDisplay(dis);
 
-  if (single) {
-    int status;
-    wait(&status);
-    ret = WEXITSTATUS(status);
-  }
+  int status;
+  wait(&status);
+  int ret = WEXITSTATUS(status);
 
   return ret;
 }
